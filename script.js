a996977420c91e7081c1d4d56fb7d78a18cce88d
sriram-65 <159551306+sriram-65@users.noreply.github.com>
2024-07-22 21:39:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+// script.js
 
+document.addEventListener('DOMContentLoaded', function () {
+    const currentHour = new Date().getHours();
+
+    if (currentHour >= 16 || currentHour < 6) {
+        document.body.classList.add('dark-mode');
+    } else {
+        document.body.classList.remove('dark-mode');
+    }
+});
